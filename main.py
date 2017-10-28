0e24773f2e7da203ff2fe708fb259ec67f2df75c
Xomak <mail@xomak.net>
2017-10-28 21:52:10 +0300

    [New] Add "Notify me"

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ if settings.SETTINGS['bot']['token'] is None:
 bot = botlab.BotLab(settings.SETTINGS)
 
 
+def build_notify_me_keyboard(session):
+    kb = telebot.types.ReplyKeyboardMarkup(row_width=1)
+
+    kb.add(telebot.types.KeyboardButton(text=session._("btn_cancel")))
+    kb.add(telebot.types.KeyboardButton(text=session._("btn_current_reminders")))
+
+    return kb
+
 def build_main_menu_keyboard(session):
     kb = telebot.types.ReplyKeyboardMarkup(row_width=1)
 
@@ -31,7 +39,25 @@ def main_menu_state(session, message):
         # TODO: Add proper keyboard here
         session.set_state("free_equipment_main")
         session.reply_message(session._("msg_free_equipment_main"))
+    elif text == session._("btn_notify_me"):
+        session.set_state("notify_me")
+        session.reply_message(session._("msg_notify_me"), reply_markup=build_notify_me_keyboard(session))
     else:
+        session.reply_message(session._("msg_main_menu"), reply_markup=build_main_menu_keyboard(session))
+
+
+@bot.message_handler(state='notify_me')
+def notify_me_state(session, message):
+    session.set_state("main_menu")
+
+    if message.text == session._("btn_current_reminders"):
+        kb = telebot.types.ReplyKeyboardMarkup(row_width=1)
+        kb.add(telebot.types.KeyboardButton(text="Washing machine 421"))
+        kb.add(telebot.types.KeyboardButton(text=session._("btn_cancel")))
+        session.reply_message("Press on notification to remove it.", reply_markup=kb)
+    else:
+        if message.text.isdigit():
+            session.reply_message(session._("msg_notification_created"))
         session.reply_message(session._("msg_main_menu"), reply_markup=build_main_menu_keyboard(session))
 
 
